a8fbfca0c63630c806b8c82ed0287a5309d5ba21
caedonhsieh <caedonhsieh@gmail.com>
2020-11-13 00:09:51 -0600

    added audio

--- scripts/app.js
+++ scripts/app.js
@@ -1,57 +1,57 @@
 var WIDTH = 300;
 var HEIGHT = 300;
+var started = false;
+var analyserNode;
 
 const canvas = document.querySelector(".visualizer");
 const canvasCtx = canvas.getContext("2d");
 
+function startAudio() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    const context = new AudioContext();
+    analyserNode = context.createAnalyser();
+    const audio = new Audio("project-22955.wav");
+    const source = context.createMediaElementSource(audio);
+    analyserNode.fftSize = 256;
+    source.connect(analyserNode);
+    source.connect(context.destination);
+    audio.play();
+}
+
 function visualize() {
     WIDTH = canvas.width;
     HEIGHT = canvas.height;
+    var bufferLength = analyserNode.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
 
-    // var visualSetting = visualSelect.value;
-    const visualSetting = "frequencybars";
-    // console.log(visualSetting);
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-    if (visualSetting == "frequencybars") {
-    //   analyser.fftSize = 256;
-        const bufferLengthAlt = 10;
-    //   var bufferLengthAlt = analyser.frequencyBinCount;
-    //   var dataArrayAlt = new Uint8Array(bufferLengthAlt);
-        const dataArrayAlt = [1, 5, 7, 4, 3, 1, 6, 4, 9, 10];
+    var drawAlt = function() {
+        drawVisual = requestAnimationFrame(drawAlt);
+
+        analyserNode.getByteFrequencyData(dataArray);
+
+        var barWidth = (WIDTH / bufferLength);
+        var barHeight;
+        var x = 0;
 
         canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+        for (var i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i] * 10;
 
-        var drawAlt = function() {
-            drawVisual = requestAnimationFrame(drawAlt);
+            canvasCtx.fillStyle = "rgb(' + (barHeight+100) + ', 50, 50)";
+            canvasCtx.fillRect(x,HEIGHT-barHeight/10,barWidth,barHeight/10);
 
-            // analyser.getByteFrequencyData(dataArrayAlt);
-
-            // canvasCtx.fillStyle = "rgb(0, 0, 0)";
-            // canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-            var barWidth = (WIDTH / bufferLengthAlt);
-            var barHeight;
-            var x = 0;
-
-            for (var i = 0; i < bufferLengthAlt; i++) {
-                barHeight = dataArrayAlt[i] * 10;
-                console.log(barHeight);
-
-                canvasCtx.fillStyle = "rgb(' + (barHeight+100) + ', 50, 50)";
-                canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
-
-                x += barWidth + 1;
-            }
-        };
-
-        drawAlt();
+            x += barWidth + 1;
+        }
     }
-    // else if (visualSetting == "off") {
-    //   canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-    //   canvasCtx.fillStyle = "red";
-    //   canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-    // }
-
+    drawAlt();
 }
 
-visualize();
+window.onclick = function() {
+    if (!started) {
+        started = true;
+        this.startAudio();
+        this.visualize();
+    }
+}
