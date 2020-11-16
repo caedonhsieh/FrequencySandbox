a7648f563bfd3def915707ab484c2e568aa0ad97
caedonhsieh <caedonhsieh@gmail.com>
2020-11-16 10:55:48 -0600

    logarithmic scale attempt 1

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,8 @@ function startAudio() {
     analyserNode = context.createAnalyser();
     const audio = new Audio("project-22955.wav");
     const source = context.createMediaElementSource(audio);
-    analyserNode.fftSize = 256;
+    console.log(context.sampleRate);
+    analyserNode.fftSize = 16384;
     source.connect(analyserNode);
     source.connect(context.destination);
     audio.play();
@@ -30,14 +31,16 @@ function visualize() {
         drawVisual = requestAnimationFrame(drawAlt);
 
         analyserNode.getByteFrequencyData(dataArray);
+        var logLength = 80;
+        var logArray = computeLogArray(dataArray);
 
-        var barWidth = (WIDTH / bufferLength);
+        var barWidth = (WIDTH / logLength);
         var barHeight;
         var x = 0;
 
         canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-        for (var i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i] * 10;
+        for (var i = 0; i < logLength; i++) {
+            barHeight = logArray[i] * 10;
 
             canvasCtx.fillStyle = "rgb(' + (barHeight+100) + ', 50, 50)";
             canvasCtx.fillRect(x,HEIGHT-barHeight/10,barWidth,barHeight/10);
@@ -46,6 +49,21 @@ function visualize() {
         }
     }
     drawAlt();
+}
+
+function computeLogArray(array) {
+    var logArray = new Uint8Array(80);
+    logArray.fill(0);
+    var li, start, offset;
+    for (var i = 8; i < array.length; i++) {
+        li = Math.floor(Math.log2(i)) - 3;
+        start = 8 * li;
+        offset = Math.floor(i/Math.pow(2, li)) - 8;
+        logArray[start+offset] = array[i]
+        // logArray[start+offset] += Math.floor(array[i]/(li+1));
+    }
+    // console.log(logArray);
+    return logArray;
 }
 
 window.onclick = function() {
